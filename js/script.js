1398d34b8b3750b29aa9b4083e87c637909703f2
FadiAlashqar <fadialshqar.fa01@gmail.com>
2025-03-20 21:46:19 +0100

    [UPD] variabili ex 1 done

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// Mail
+// Crea una lista di email di invitati ad una festa. Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo, utilizzando un ciclo for. Non è consentito usare nessun metodo proprio degli array (come includes, per esempio). Si può fare? Certo che si basta ragionare un po’. Nota: Non è necessario provvedere alla validazione delle email
+
+// creo un array contenente le mail degli invitati:
+let mail = ["giacomo@mail.com, fiorello@mail.com, valentina@mail.com, anna@mail.com, lisa@mail.com, giovanni@mail.com, giulia@mail.com"];
+// creo una variabile prompt dove andare ad inserire la mail dell'utente:
+let enterMail = prompt("inserisci la tua mail")
+let guest;
